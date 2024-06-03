79a4c6da7412dcdc9e2ca8a23c035999ce71334a
julia-lahtina <j.lakhtin@gmail.com>
2024-06-03 19:38:01 +0200

    refactoring: thunk fetchTasks

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const settings = {
   withCredentials: true,
   headers: {
-    "API-KEY": "1cdd9f77-c60e-4af5-b194-659e4ebd5d41",
+    "API-KEY": "9778ccc8-038a-4d81-9130-86ef29c12736",
   },
 };
 const instance = axios.create({
