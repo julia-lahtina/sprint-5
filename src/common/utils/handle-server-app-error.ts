9aae4e49c131b10d90ccac4f12cf5c6da8fbd1c0
julia-lahtina <j.lakhtin@gmail.com>
2024-06-10 13:05:09 +0200

    error handling

--- src/common/utils/handle-server-app-error.ts
+++ src/common/utils/handle-server-app-error.ts
@@ -1,12 +1,14 @@
 import { Dispatch } from "redux";
 import { appActions } from "app/app.reducer";
-import { ResponseType } from "common/types/common.types";
+import { BaseResponse } from "common/types/common.types";
 
-export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch) => {
-  if (data.messages.length) {
-    dispatch(appActions.setAppError({ error: data.messages[0] }));
-  } else {
-    dispatch(appActions.setAppError({ error: "Some error occurred" }));
+export const handleServerAppError = <D>(data: BaseResponse<D>, dispatch: Dispatch, isGlobalError: boolean = true) => {
+  if (isGlobalError) {
+    if (data.messages.length) {
+      dispatch(appActions.setAppError({ error: data.messages[0] }));
+    } else {
+      dispatch(appActions.setAppError({ error: "Some error occurred" }));
+    }
   }
   dispatch(appActions.setAppStatus({ status: "failed" }));
 };
--- src/features/auth/auth.reducer.ts
+++ src/features/auth/auth.reducer.ts
@@ -36,7 +36,8 @@ const login = createAppAsyncThunk<{ isLoggedIn: boolean }, LoginParamsType>(
         dispatch(appActions.setAppStatus({ status: "succeeded" }));
         return { isLoggedIn: true };
       } else {
-        handleServerAppError(res.data, dispatch);
+        // dispatch(appActions.setAppStatus({ status: "failed" }));
+        handleServerAppError(res.data, dispatch, false);
         return rejectWithValue(res.data);
       }
     } catch (e) {
