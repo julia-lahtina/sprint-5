721a05e05e0be940667c318d14c6976f6f0741bd
julia-lahtina <j.lakhtin@gmail.com>
2024-06-10 11:59:41 +0200

    change common type

--- src/common/types/common.types.ts
+++ src/common/types/common.types.ts
@@ -1,5 +1,18 @@
-export type ResponseType<D = {}> = {
+/* export type BaseResponse<D = {}> = {
   resultCode: number;
   messages: Array<string>;
   data: D;
+}; */
+
+export type FieldError = {
+  error: string;
+  field: string;
+};
+
+//❗ Чтобы у нас не было пересечения имен назовем общий тип BaseResponseType
+export type BaseResponse<D = {}> = {
+  resultCode: number;
+  messages: string[];
+  data: D;
+  fieldsErrors: FieldError[];
 };
